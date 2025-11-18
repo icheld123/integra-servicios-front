de79f38f34ea8654e5c054ed198ca34164f2f593
icheld123 <icheldmorales@gmail.com>
2025-11-18 12:29:38 -0500

    Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { InicioComponent } from './feature/inicio/inicio.component';
+import { RegistroComponent } from './feature/registro/registro.component';
+import { InicioSesionComponent } from './feature/inicio-sesion/inicio-sesion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    InicioComponent,
+    RegistroComponent,
+    InicioSesionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
